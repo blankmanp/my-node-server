e816d24a446016ee5f8c614ead46ab23c0181519
blankmanp <blankman.p@gmail.com>
2016-02-17 19:19:44 +0800

    first commit 20160217

--- gulp/config.js
+++ gulp/config.js
@@ -2,17 +2,17 @@
 * @Author: pengyanxin
 * @Date:   2016-01-15 10:59:47
 * @Last Modified by:   blankmanp
-* @Last Modified time: 2016-01-26 14:05:35
+* @Last Modified time: 2016-02-16 16:26:27
 */
 
 export default {
-    src: 'src/',
+    src: '../src/examples/',
     dest: 'dest',
     js: {
-        src: 'src/js/'
+        src: '../src/js/'
     },
     css: {
-        src: 'src/less/'
+        src: '../src/less/'
     },
     assets: 'assets/'
 };
