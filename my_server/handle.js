06b2c517d328aaec89a5475d9884f3ed518948ca
blankmanp <blankman.p@gmail.com>
2016-02-26 12:08:29 +0800

    修改了静态文件的读取方式

--- my_server/handle.js
+++ my_server/handle.js
@@ -2,7 +2,7 @@
 * @Author: blankmanp
 * @Date:   2016-02-22 11:49:29
 * @Last Modified by:   blankmanp
-* @Last Modified time: 2016-02-22 15:29:17
+* @Last Modified time: 2016-02-26 11:40:18
 */
 
 'use strict';
@@ -15,53 +15,30 @@ const querystring = require('querystring');
 const _ = require('lodash');
 const formidable = require('formidable');
 
+let staticSource = './static';
 let cache = {};
 let mimeTypes = {
     '.css': 'text/css',
     '.js': 'text/javascript',
     '.html': 'text/html'
 };
+let filePath = {
+    '.css': `${staticSource}/css`,
+    '.js': `${staticSource}/js`
+}
 
 let handle = {
     index: {
         _render: (request, response) => {
-            let link = url.parse(decodeURI(request.url));
-            let file = link.pathname;
-            file.indexOf('.') === -1 && (file += '.html');
-            let filePath = `./static/${file}`;
-            fs.stat(filePath, (err, stats) => {
-                if (err) {
-                    response.writeHead(404);
-                    response.end('' + err);
-                    return;
-                }
-                let updateTime = Date.parse(stats.ctime);
-                let isUpdated = cache[file] && cache[file]._timeStamp < updateTime;
-                if (!cache[file] || isUpdated) {
-                    console.log(`read ${file} from file`)
-                    fs.readFile(filePath, (err, data) => {
-                        if (err) {
-                            response.end('' + err);
-                            return;
-                        }
-                        cache[file] = {
-                            _content: data,
-                            _timeStamp: Date.now()
-                        };
-                        response.writeHead(200, {'Content-Type': mimeTypes[path.extname(file)]});
-                        response.end(data);
-                    })
-                    return;
-                }
-                console.log(`read ${file} from cache`);
-                response.writeHead(200, {'Content-Type': mimeTypes[path.extname(file)]});
-                response.end(cache[file]._content);
-            })
+            readFileFromCache(request, response);
         },
 
         uploadFile: {
             _render: (request, response) => {
+                console.log(request.method);
                 if (request.method !== 'POST') {
+                    response.writeHead(404);
+                    response.end('The page don\'t exist');
                     return;
                 }
                 let form = new formidable.IncomingForm();
@@ -77,8 +54,83 @@ let handle = {
                 });
                 form.parse(request);
             }
+        },
+
+        download: {
+            _render: (request, response) => {
+                let options = {};
+                options.file = '50meg';
+                options.fileSize = fs.statSync(options.file).size;
+                options.kbs = 32;
+                let download = Object.create(options);
+                download.chunk = new Buffer(options.fileSize);
+                download.bufferOffset = 0;
+                response.writeHead(200, {'Content-Length': options.fileSize});
+                fs.createReadStream(options.file)
+                .on('data', (chunk) => {
+                    chunk.copy(download.chunk, download.bufferOffset);
+                    download.bufferOffset += chunk.length;
+                    response.write(`${download.bufferOffset}\n`);
+                })
+                .once('open', () => {
+                })
+                .on('end', () => {
+                    response.end('done');
+                })
+                // function throttle(download, callback) {
+                //     let chunkOutSize = download.kbps * 1024;
+                //     let timer = 0;
+                //     (function loop(byteSent) {
+                //         if (!download.aborted) {
+                //             setTimeout(() => {
+                //                 let byteOut = byteSent + chunkOutSize;
+                //                 if (download.bufferOffset > byteOut) {
+
+                //                 }
+                //             })
+                //         }
+                //     }(0))
+                // }
+            }
         }
     }
+}
+
+function readFileFromCache(request, response) {
+    let link = url.parse(decodeURI(request.url));
+    let file = link.pathname;
+    console.log(link);
+    file.indexOf('.') === -1 && (file += '/index.html');
+    let filePath = `./static/${file}`;
+    fs.stat(filePath, (err, stats) => {
+        if (err) {
+            console.log(`can't read ${filePath}`);
+            response.writeHead(404);
+            response.end('' + err);
+            return;
+        }
+        let updateTime = Date.parse(stats.ctime);
+        let isUpdated = cache[file] && cache[file]._timeStamp < updateTime;
+        if (!cache[file] || isUpdated) {
+            console.log(`read ${file} from file`)
+            fs.readFile(filePath, (err, data) => {
+                if (err) {
+                    response.end('' + err);
+                    return;
+                }
+                cache[file] = {
+                    _content: data,
+                    _timeStamp: Date.now()
+                };
+                response.writeHead(200, {'Content-Type': mimeTypes[path.extname(file)]});
+                response.end(data);
+            })
+            return;
+        }
+        console.log(`read ${file} from cache`);
+        response.writeHead(200, {'Content-Type': mimeTypes[path.extname(file)]});
+        response.end(cache[file]._content);
+    })
 }
 
 module.exports = handle;
--- my_server/route.js
+++ my_server/route.js
@@ -2,21 +2,53 @@
 * @Author: blankmanp
 * @Date:   2016-02-22 11:49:18
 * @Last Modified by:   blankmanp
-* @Last Modified time: 2016-02-22 15:10:03
+* @Last Modified time: 2016-02-26 12:06:24
 */
 
 'use strict';
 
 const url = require('url');
-const handle = require('./handle')
+const handle = require('./handle');
+const path = require('path');
+const fs = require('fs');
+const staticSource = './static';
+let mimeTypes = {
+    '.css': 'text/css',
+    '.js': 'text/javascript'
+};
+const filePath = {
+    '.css': `${staticSource}/css`,
+    '.js': `${staticSource}/js`,
+}
 
 
 function route(req, res) {
     let link = url.parse(decodeURI(req.url));
-    let pathname = link.pathname.substring(1).split('/');
+    let pathname = path.normalize(link.pathname);
     let temp = handle;
     let wrongInfo = {};
+    if (filePath[path.extname(pathname)]) {
+        let tempFile = filePath[path.extname(pathname)]
+        fs.stat(`${tempFile}/${pathname}`, (err, stats) => {
+            if (err) {
+                res.writeHead(404);
+                res.end('' + err);
+                return;
+            }
+            fs.readFile(`${tempFile}/${pathname}`, (err, data) => {
+                if (err) {
+                    res.end('' + err);
+                    return;
+                }
+                res.writeHead(200, {'Content-Type': mimeTypes[path.extname(pathname)]});
+                res.end(data);
+            });
+        })
+        return;
+    }
+    pathname = pathname.substring(1).split('/');
     if (pathname[0] === 'favicon.ico') {
+        res.end();
         return;
     }
     pathname.forEach((v) => {
